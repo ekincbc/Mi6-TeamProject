e4e6891ca8155ff358cdccd2ef46188cc0c3f102
Lucian Nguyen <lucian.n@seznam.cz>
2022-03-14 11:09:54 +0100

    filled relationships in Alias,Image,Status

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Person;
+
 
 class Status extends Model
 {
+
+    public function status()
+
+        {
+            return $this->hasMany(Person::class);
+        }
     use HasFactory;
 }
